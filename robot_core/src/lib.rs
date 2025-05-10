64b4d5159b39b9e59f23003647d4b95faaee41ed
Skienex <106771120+Skienex@users.noreply.github.com>
2025-05-10 11:36:25 +0200

    feat: implemented gpio control for extra features: - horn - lights
    and implemented turbo logic for temporary speed boost

--- robot_core/src/lib.rs
+++ robot_core/src/lib.rs
@@ -1,5 +1,6 @@
-use log::{error, info, warn};
+use log::{error, info, log, warn};
 use pca9685_rppal::Pca9685;
+use rppal::gpio::Gpio;
 use robot_web::CommandPayload;
 use std::sync::mpsc;
 use std::thread;
@@ -26,10 +27,16 @@ pub fn main() {
         error!("[Main Thread] Motor Controller not initialized (perhaps not plugged in?!)")
     }
 
+    let mut turbo = false;
+
     let mut controller = controller_res.unwrap();
     controller.init().unwrap();
 
     controller.set_pwm_freq(FREQ).unwrap();
+
+    let gpio = Gpio::new().unwrap();
+    let mut horn = gpio.get(23).unwrap().into_output();
+    let mut lights = gpio.get(24).unwrap().into_output();
 
     loop {
         match rx.recv() {
@@ -40,17 +47,53 @@ pub fn main() {
                 );
                 match command_payload.command.as_str() {
                     "speed" => {
-                        if let Some(i) = command_payload.value.as_i64() {
+                        if let Some(s) = command_payload.value.as_i64() {
                             info!("[Main Thread] Successfully received speed value");
-                            controller.set_pwm(0, 0, speed_to_pulse(i)).unwrap()
+                            controller.set_pwm(0, 0, speed_to_pulse(s, turbo)).unwrap()
                         } else {
                             info!("[Main Thread] No speed value provided")
                         }
                     }
-                    "direction" => {}
-                    "headlights" => {}
-                    "horn" => {}
-                    "turbo" => {}
+                    "direction" => {
+                        if let Some(d) = command_payload.value.as_i64() {
+                            info!("[Main Thread] Successfully received direction value");
+                            controller.set_pwm(0, 0, direction_to_pulse(d)).unwrap(); // TODO: Direction to pulse
+                        } else {
+                            info!("[Main Thread] No direction value provided")
+                        }
+                    }
+                    "headlights" => {
+                        if let Some(h) = command_payload.value.as_bool() {
+                            info!("[Main Thread] Successfully received headlights value");
+                            if h {
+                                lights.set_high();
+                            } else {
+                                lights.set_low();
+                            }
+                        } else {
+                            info!("[Main Thread] No headlights value provided")
+                        }
+                    }
+                    "horn" => {
+                        if let Some(h) = command_payload.value.as_bool() {
+                            info!("[Main Thread] Successfully received horn value");
+                            if h {
+                                horn.set_high();
+                            } else {
+                                horn.set_low();
+                            }
+                        } else {
+                            info!("[Main Thread] No horn value provided")
+                        }
+                    }
+                    "turbo" => {
+                        if let Some(t) = command_payload.value.as_bool() {
+                            info!("[Main Thread] Successfully received turbo value");
+                            turbo = t;
+                        } else {
+                            info!("[Main Thread] No turbo value provided")
+                        }
+                    }
                     "calibrate" => {}
                     _ => {}
                 }
@@ -73,7 +116,7 @@ pub fn main() {
     info!("[Main Thread] Anwendung wird beendet.");
 }
 
-fn speed_to_pulse(speed: i64) -> u16 {
+fn speed_to_pulse(speed: i64, turbo: bool) -> u16 {
     // Clamp auf erlaubten Bereich
     let x = speed.clamp(-100, 100);
 
@@ -82,10 +125,21 @@ fn speed_to_pulse(speed: i64) -> u16 {
     } else if x < -7 {
         let slope = 200.0 / 93.0;
         let y = 1450.0 + (x as f32 + 7.0) * slope;
+        if turbo {
+            return y.round() as u16 - 100;
+        }
         y.round() as u16
     } else {
-        let slope = 850.0 / 93.0;
+        let slope = 750.0 / 93.0;
         let y = 1450.0 + (x as f32 - 7.0) * slope;
+        if turbo {
+            return y.round() as u16 + 100;
+        }
         y.round() as u16
     }
+}
+
+fn direction_to_pulse(direction: i64) -> u16 {
+    let _ = direction.clamp(-100, 100);
+    todo!()
 }
